c49b4e2d185437ad6438a568034dc9a468949ee4
Adam Goodchild <adam@creativegaming.co>
2019-10-03 16:56:57 +0100

    Updates

---
+++ Assets/Scripts/TestSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawner : MonoBehaviour
+{
+    #region Public Variables
+
+    public GameObject Prefab;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        Instantiate(Prefab, Vector3.zero, Quaternion.identity);
+    }
+
+    #endregion
+}
